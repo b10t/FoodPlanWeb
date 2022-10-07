9b72569caacacee616e067aa016c09fe3a0bdd20
CaDiBob <nikolay.v.byankin@gmail.com>
2022-10-07 16:28:59 +0900

    Обновил хосты для прода

--- FoodPlan/settings/prod_settings.py
+++ FoodPlan/settings/prod_settings.py
@@ -1,4 +1,4 @@
 from FoodPlan.settings.default_settings import *
 
 
-ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', ['0.0.0.0', '81.163.26.170', 'localhost'])
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', ['0.0.0.0', '81.163.26.170', 'localhost', 'foodplandvm.tk'])
