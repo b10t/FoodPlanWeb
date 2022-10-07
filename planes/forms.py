e5b74a360b82b6d7c696e0363c761220ee37bfba
b10t <b10t.rus@gmail.com>
2022-10-07 10:53:59 +0700

    Change user data.

--- planes/forms.py
+++ planes/forms.py
@@ -1,9 +1,32 @@
+from django import forms
 from django.contrib.auth import get_user_model
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserChangeForm, UserCreationForm
+from django.core.exceptions import ValidationError
 
 
 class CustomUserCreationForm(UserCreationForm):
 
     class Meta:
         model = get_user_model()
+        fields = ('username', 'email')
+
+
+class CustomUserChangeForm(UserChangeForm):
+    password1 = forms.CharField(required=False)
+    password2 = forms.CharField(required=False)
+
+    password = None
+
+    def clean(self):
+        super().clean()
+        password1 = self.cleaned_data['password1']
+        password2 = self.cleaned_data['password2']
+        if password1 and password2 and password1 != password2:
+            errors = {'password2': ValidationError(
+                'Введённые пароли не совпадают', code='password_mismatch')}
+            raise ValidationError(errors)
+
+    class Meta:
+        model = get_user_model()
+
         fields = ('username', 'email')
--- planes/views.py
+++ planes/views.py
@@ -1,9 +1,10 @@
+from django.contrib.auth import update_session_auth_hash
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import get_object_or_404, redirect, render
 from django.urls import reverse_lazy
 from django.views.generic.edit import CreateView
 
-from .forms import CustomUserCreationForm
+from .forms import CustomUserChangeForm, CustomUserCreationForm
 
 
 class SignUpView(CreateView):
@@ -21,7 +22,39 @@ def index(request):
 
 @login_required
 def personal_account(request):
+    user = request.user
+    message = ''
+
+    if request.method == 'POST':
+        form = CustomUserChangeForm(request.POST, instance=request.user)
+
+        # Костыль из-за disabled в input
+        form.errors.pop('email', None)
+
+        form_data = form.data.copy()
+        form_data['email'] = user.email
+
+        form.data = form_data
+
+        if form.is_valid():
+            user = form.save()
+
+            message = 'Данные успешно изменены.'
+
+            if password := form.cleaned_data.get('password1'):
+                user.set_password(password)
+                user.save()
+
+                update_session_auth_hash(request, user)
+
+                message = 'Пароль успешно изменён.'
+
+    else:
+        form = CustomUserChangeForm()
+
     context = {
+        'form': form,
+        'message': message,
     }
 
     return render(request, 'lk.html', context)
