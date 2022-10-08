ae7504fcebb502e4084832c19b1f08ecb97f1245
CaDiBob <nikolay.v.byankin@gmail.com>
2022-10-09 00:31:10 +0900

    Упростил модель Пользователя

--- planes/admin.py
+++ planes/admin.py
@@ -1,25 +1,22 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin
 
 from planes.models import (
-    User,
     MenuType,
     Allergy,
     Dish,
     DishIngredient,
     Subscribe
-    )
+)
+
+User = get_user_model()
 
 
 @admin.register(User)
-class UserAdmin(admin.ModelAdmin):
-    list_display = [
-        'email',
-        'username',
-        'is_staff',
-        'is_active',
-        'is_admin',
-    ]
-    search_fields = ('username', 'email')
+class UserAdmin(UserAdmin):
+    pass
+
 
 @admin.register(MenuType)
 class MenuTypeAdmin(admin.ModelAdmin):
